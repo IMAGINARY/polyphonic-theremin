d8159d1f59e58be6ee3dd47ac2cad381b1acc2ab
Christian Stussak <stussak@mfo.de>
2022-12-23 00:00:57 +0100

    Add initial prototype of MusKI component

---
+++ src/muski/ts/index.ts
@@ -0,0 +1,43 @@
+import cssText from 'bundle-text:../../lib/scss/polyphonic-theremin.module.scss';
+import PolyphonicTheremin, {
+  PolyphonicThereminOptions,
+  ToneOptions,
+} from '../../lib/ts';
+
+function migrateDataAttribute<T>(
+  o: Record<string, unknown>,
+  key: string,
+  e: HTMLElement,
+  coerce: (s: string) => T,
+) {
+  const attributeValue = e.dataset[key];
+  if (typeof attributeValue !== 'undefined') {
+    // eslint-disable-next-line no-param-reassign
+    o[key] = coerce(attributeValue);
+  }
+}
+
+function initMuskiThereminComponent(e: HTMLElement) {
+  const m = migrateDataAttribute;
+
+  const o: Partial<PolyphonicThereminOptions> = {};
+  m(o, 'touchElementCssClasses', e, (s: string) => s.split(' '));
+  m(o, 'touchElementStyle', e, (s: string) => s);
+
+  const to: Partial<ToneOptions> = {};
+  m(to, 'waveType', e, (s: string) => s);
+  m(to, 'gainMin', e, Number.parseFloat);
+  m(to, 'gainMax', e, Number.parseFloat);
+  m(to, 'frequencyMinHz', e, Number.parseFloat);
+  m(to, 'frequencyMaxHz', e, Number.parseFloat);
+  m(to, 'attackMs', e, Number.parseFloat);
+  m(to, 'releaseMs', e, Number.parseFloat);
+  m(to, 'updateMs', e, Number.parseFloat);
+  m(to, 'mute', e, (s: string) => true);
+
+  const polyphonicTheremin = new PolyphonicTheremin(e, o, to);
+}
+
+document
+  .querySelectorAll<HTMLElement>('*[data-component="muski-theremin"]')
+  .forEach(initMuskiThereminComponent);
