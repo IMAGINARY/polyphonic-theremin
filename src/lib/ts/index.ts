635e0ff34f0eef9fafdbaf19a7c4c4041e910513
Christian Stussak <stussak@mfo.de>
2022-12-22 18:53:49 +0100

    Experiment with TypeScript module resolution
    This is still work in progress.

--- src/lib/ts/index.ts
+++ src/lib/ts/index.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="declarations.d.ts"/>
 import PolyphonicTheremin from './polyphonic-theremin';
 import type {
   // Workaround because TypeScript currently can't re-export types via "export * from '...'"
